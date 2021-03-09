6e869144c1ad2313fbceb1c654e813832bd3b403
Nipun Jain <lucifernipun22@gmail.com>
2021-03-10 02:17:24 +0530

    "first commit"

---
+++ app/src/main/java/com/example/okcredit/Views/SplashScreen1.java
@@ -0,0 +1,28 @@
+package com.example.okcredit.Views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.okcredit.R;
+
+import okhttp3.internal.http2.Http2Reader;
+
+public class SplashScreen1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen1);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i=new Intent(SplashScreen1.this,SplashScreen2.class);
+                startActivity(i);
+            }
+        }, 1000);
+    }
+}
