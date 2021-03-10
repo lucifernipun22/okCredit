1ebb2f8e1cfa1d750e489880af2a0b1e1e993c0e
sarika1609 <sarika.thota1992@gmail.com>
2021-03-10 11:18:19 +0530

    working on home hape of adding customer updated manifest files for home

--- app/src/main/java/com/example/okcredit/Repository/classRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.okcredit.Repository;
-
-import com.example.okcredit.Data.CustomerEntity;
-import com.example.okcredit.Data.OkCreditDAO;
-
-import kotlinx.coroutines.Dispatchers;
-
-class classRepo {
-}
--- app/src/main/java/com/example/okcredit/ViewModel/viewmodelclass.kt
+++
@@ -1,4 +0,0 @@
-package com.example.okcredit.ViewModel
-
-class viewmodelclass {
-}
