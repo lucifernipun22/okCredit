e2c2e8d15b3aab056b5050da4ac8f4fcacd9f1fe
Ishwar Kumar <kumarishwar623@gmail.com>
2021-03-10 12:07:56 +0530

    animation

--- app/src/main/java/com/example/okcredit/Views/LanguageSelect.java
+++ app/src/main/java/com/example/okcredit/Views/LanguageSelect.java
@@ -31,42 +31,43 @@ public class LanguageSelect extends AppCompatActivity {
         btnEnglish = findViewById(R.id.languageEnglish);
         tvWelcome = findViewById(R.id.welcome);
         tvSelectLanguage = findViewById(R.id.selectLanguage);
-        /*btnNext.setOnClickListener(new View.OnClickListener() {
+        btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LanguageSelect.this, IntroductionActivity.class);
                 startActivity(intent);
             }
         });
-        btnHindi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                changeLanguage("hn");
-            }
-        });
-        btnEnglish.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                changeLanguage("en");
-            }
-        });
-
-
-
-    }
-    private void changeLanguage(String language){
-        Locale locale = new Locale(language);
-        Locale.setDefault(locale);
-        Configuration configuration = new Configuration();
-        configuration.locale = locale;
-        getApplicationContext().getResources().updateConfiguration(configuration,getApplicationContext().getResources().getDisplayMetrics());
-
-        tvWelcome.setText(getString(R.string.welcome));
-        tvSelectLanguage.setText(getString(R.string.select_your_language));
-        btnNext.setText(getString(R.string.next));
-
-    }*/
+//        btnHindi.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                changeLanguage("hn");
+//            }
+//        });
+//        btnEnglish.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                changeLanguage("en");
+//            }
+//        });
+//
+//
+//
+//    }
+//    private void changeLanguage(String language){
+//        Locale locale = new Locale(language);
+//        Locale.setDefault(locale);
+//        Configuration configuration = new Configuration();
+//        configuration.locale = locale;
+//        getApplicationContext().getResources().updateConfiguration(configuration,getApplicationContext().getResources().getDisplayMetrics());
+//
+//        tvWelcome.setText(getString(R.string.welcome));
+//        tvSelectLanguage.setText(getString(R.string.select_your_language));
+//        btnNext.setText(getString(R.string.next));
+//
+//    }
     }
 }
