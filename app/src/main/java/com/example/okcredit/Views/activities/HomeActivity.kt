dd94d77b0cf4d7b092d7767e5527d66c211cecf4
sachinsmrat123 <36136410+sachinsmrat123@users.noreply.github.com>
2021-03-13 12:38:06 +0530

    commit

--- app/src/main/java/com/example/okcredit/Views/activities/HomeActivity.kt
+++ app/src/main/java/com/example/okcredit/Views/activities/HomeActivity.kt
@@ -1,17 +1,36 @@
 package com.example.okcredit.Views.activities
-
+import android.os.Bundle
+import android.view.Menu
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import android.content.Intent
 import android.os.Bundle
 import android.provider.Settings.Global.getString
 import android.view.LayoutInflater
+import android.view.Menu
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.NavHostFragment.findNavController
+
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import com.bumptech.glide.load.engine.Resource
 import com.example.okcredit.Data.local.Customer
 import com.example.okcredit.Data.local.OkCreditDAO
 import com.example.okcredit.Data.local.Supplier
 import com.example.okcredit.Data.local.User
 import com.example.okcredit.R
+import com.example.okcredit.R.id.nav_host_fragment
 import com.example.okcredit.Repository.OkCreditRepo
 import com.example.okcredit.ViewModel.CustomerViewModel
 import com.example.okcredit.ViewModel.CustomerViewModelFactory
@@ -24,6 +43,8 @@ import com.example.okcredit.Views.values.OkCreditApplication
 import com.example.okcredit.Views.values.Prefs.getString
 import com.example.okcredit.Views.values.SharedPref
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.navigation.NavigationView
+
 import com.san.app.activity.BaseActivity
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.bottom_sheet_layout.*
@@ -31,153 +52,51 @@ import kotlinx.android.synthetic.main.customer_item_layout.view.*
 import org.greenrobot.eventbus.EventBus
 
 
-class HomeActivity : BaseActivity(), OnRowItemClicked {
 
-    lateinit var okCreditDao: OkCreditDAO
-    lateinit var customerViewModel: CustomerViewModel
-    lateinit var okCreditRepository: OkCreditRepo
-    var radioButtonClick: Int? = null
 
-    private lateinit var customerList: MutableList<Customer>
-    private lateinit var customerAdapter: CustomerAdapter
-    private lateinit var pagerAdapter: ViewPagerFragmentAdapter
-    private lateinit var user: User
+class HomeActivity : AppCompatActivity(), OnRowItemClicked {
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
-        setViewPagerAdapter()
-        initializations()
-        initViews()
-
-        etMaterialSearch.setOnClickListener {
-            startActivity()
-        }
-
-//        btnAddCustomer.setOnClickListener {
-//            intent = Intent(this, AddCustomerActivity::class.java)
-//            startActivity(intent)
+//        val fab: FloatingActionButton = findViewById(R.id.fab)
+//        fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show()
 //        }
-
-//        btnAddFilter.setOnClickListener {
-//            bottomSheetDialog()
-//        }
-
-
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.nav_home,R.id.nav_account,
+            R.id.nav_gallery, R.id.nav_slideshow,R.id.nav_helpandSupport), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 
-    private fun initViews() {
-        customerList = mutableListOf()
-
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
     }
 
-//    private fun bottomSheetDialog() {
-//
-//        val bottomSheetDialog = BottomSheetDialog(this, R.style.BottomSheetDialogTheme)
-//        val view = LayoutInflater.from(applicationContext).inflate(
-//            R.layout.bottom_sheet_layout, findViewById(R.id.llBottomConatainer)
-//        )
-//        bottomSheetDialog.setContentView(view)
-//        bottomSheetDialog.setCanceledOnTouchOutside(true)
-//        bottomSheetDialog.show()
-//
-//        val mradionName = bottomSheetDialog.findViewById<RadioButton>(R.id.radioName)
-//        val mradionAmount = bottomSheetDialog.findViewById<RadioButton>(R.id.radioAmount)
-//        val mradioLatest = bottomSheetDialog.findViewById<RadioButton>(R.id.radioLatest)
-//        val mradioGroup = bottomSheetDialog.findViewById<RadioGroup>(R.id.radioGroup)
-//
-//        val mbtnApplyBSD = bottomSheetDialog.findViewById<TextView>(R.id.btnApplyBSD)
-//        val mbtnClear = bottomSheetDialog.findViewById<TextView>(R.id.tvClearBSD)
-//        val mbtnCancel = bottomSheetDialog.findViewById<TextView>(R.id.btnCancelBSD)
-//        val mbtnTodayBDS = bottomSheetDialog.findViewById<TextView>(R.id.btnTodayBDS)
-//        val mbtnPendingSD = bottomSheetDialog.findViewById<TextView>(R.id.btnPendingSD)
-//        val btnUpcomingBSD = bottomSheetDialog.findViewById<TextView>(R.id.btnUpcomingBSD)
-//
-//        mradionAmount?.setOnClickListener {
-//            if (mradionAmount != null) {
-//                if (!mradionAmount.isChecked) {
-//                    mradionAmount.isChecked = true
-//                }
-//            }
-//
-//            if (mradionName != null) {
-//                if (mradionName.isChecked) {
-//                    mradionName.isChecked = false
-//                }
-//            }
-//
-//            if (mradioLatest != null) {
-//                if (mradioLatest.isChecked) {
-//                    mradioLatest.isChecked = false
-//                }
-//            }
-//        }
-//
-//        mbtnCancel?.setOnClickListener {
-//            bottomSheetDialog.cancel()
-//        }
-//
-////        mbtnApplyBSD?.setOnClickListener {
-////
-////            if (mradioLatest?.isChecked!!) {
-////                SharedPref.writeIntToPref(Const.RADIO_BUTTOM, 3)
-////                EventBus.getDefault().post(NewTodoItemEvent(3))
-////                bottomSheetDialog.cancel()
-////            } else if (mradionAmount?.isChecked!!) {
-////                SharedPref.writeIntToPref(Const.RADIO_BUTTOM, 2)
-////                EventBus.getDefault().post(NewTodoItemEvent(2))
-////                bottomSheetDialog.cancel()
-////            } else if (mradionName?.isChecked!!) {
-////                SharedPref.writeIntToPref(Const.RADIO_BUTTOM, 1)
-////                EventBus.getDefault().post(NewTodoItemEvent(1))
-////                bottomSheetDialog.cancel()
-////            }
-////        }
-//    }
-
-    /*private fun initializeCustomerRecyclerView() {
-        customerAdapter = CustomerAdapter(customerList, this)
-        customerAdapter.setOnItemClickListener(object :
-            CustomerAdapter.OnItemClickListener {
-            override fun onItemClick(view: View, position: Int) {
-                val c = customerList[position]
-                Log.d("Home", "onItemClick pos $position name $c")
-                gotoCustomerScreen(view, c)
-            }
-        })
-
-    }*/
-
-
-    private fun startActivity() {
-        val intent = Intent(this, FindCustomerActivity::class.java)
-        startActivity(intent)
-    }
-
-    private fun initializations() {
-        val myapplication = application as OkCreditApplication
-        okCreditDao = myapplication.okCreditDAO
-        okCreditRepository = myapplication.repository
-        val viewModelFactory = CustomerViewModelFactory(okCreditRepository)
-        customerViewModel =
-            ViewModelProviders.of(this, viewModelFactory).get(CustomerViewModel::class.java)
-    }
-
-
-    private fun setViewPagerAdapter() {
-        pagerAdapter = ViewPagerFragmentAdapter(supportFragmentManager)
-        home_viewPager.setAdapter(pagerAdapter)
-        tabLayout.setupWithViewPager(home_viewPager)
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
     override fun onItemClick(model: Customer) {
-
+        TODO("Not yet implemented")
     }
 
     override fun onItem(model: Supplier) {
-
+        TODO("Not yet implemented")
     }
-
 
 }
